c30d75677d77fce6a9ff19f0ae9ffe013fd3f5a7
Emily Featherston <109819582+emvestigates@users.noreply.github.com>
2025-03-24 12:01:56 -0400

    Add files via upload

---
+++ .github/workflows/doge_scraper.R
@@ -0,0 +1,19 @@
+library(RSelenium)
+library(rvest)
+library(purrr)
+driver <- remoteDriver(
+  remoteServerAddr = "localhost",
+  port = 4444,
+  browserName = "chrome"
+)
+driver$open()
+driver$navigate("https://doge.gov/savings")
+button <- remote_driver$findElement(using = "xpath", value = "/html/body/div/main/div/div/div[5]/div[2]/div/div/button")
+button$click() 
+Sys.sleep(3)
+page_source <- remote_driver$getPageSource()[[1]]
+page <- LinkExtractor(page_source, ExternalLInks = TRUE)
+fpds <- page$ExternalLinks
+links_df <- data.frame(links = fpds)
+write.csv(links_df, "scraped_links.csv", row.names = FALSE)
+driver$close()

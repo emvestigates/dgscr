9676acc790534eadf62085cbe58666fc7caad842
Emily Featherston <109819582+emvestigates@users.noreply.github.com>
2025-03-24 12:25:44 -0400

    Update doge_scraper.R

--- .github/workflows/doge_scraper.R
+++ .github/workflows/doge_scraper.R
@@ -8,10 +8,10 @@ driver <- remoteDriver(
 )
 driver$open()
 driver$navigate("https://doge.gov/savings")
-button <- remote_driver$findElement(using = "xpath", value = "/html/body/div/main/div/div/div[5]/div[2]/div/div/button")
+button <- driver$findElement(using = "xpath", value = "/html/body/div/main/div/div/div[5]/div[2]/div/div/button")
 button$click() 
 Sys.sleep(3)
-page_source <- remote_driver$getPageSource()[[1]]
+page_source <- driver$getPageSource()[[1]]
 page <- LinkExtractor(page_source, ExternalLInks = TRUE)
 fpds <- page$ExternalLinks
 links_df <- data.frame(links = fpds)
